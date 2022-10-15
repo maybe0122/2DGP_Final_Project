07a90e25d91511c50959c72e995ae76230560ab4
kyuminKim <kyuminkim0122@gmail.com>
2022-10-16 04:21:35 +0900

    correction main

--- Final_Project/main.py
+++ Final_Project/main.py
@@ -1,9 +1,10 @@
 import play_state
 import pico2d
 import title_state
+import logo_state
 
-start_state = [title_state, play_state]
-pico2d.open_canvas()
+start_state = [logo_state, title_state, play_state]
+pico2d.open_canvas(800, 950)
 
 
 for state in start_state:
