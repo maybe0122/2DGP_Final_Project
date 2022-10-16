ed501cd4bb30605d2a00af2feb22a8dfd883821f
kyuminKim <kyuminkim0122@gmail.com>
2022-10-16 20:40:04 +0900

    correction menu_state
    menu_state 기본 틀 내용 추가 / 미완

--- Final_Project/menu_state.py
+++ Final_Project/menu_state.py
@@ -1,7 +1,31 @@
 from pico2d import *
 import game_framework
+import title_state
+import count_state
+import play_state
+
+image = None
+new = False
+
+def enter():
+    global image
+    image = load_image('')  # add image later
+
+def exit():
+    global image
+    del image
+
+def update():
+    pass
+
+def draw():
+    clear_canvas()
+    play_state.draw_world()
+    image.draw(800, 950)
+    update_canvas()
 
 def handle_events():
+    global new
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -9,8 +33,16 @@ def handle_events():
         elif event.type == SDL_KEYDOWN:
             match event.key:
                 case pico2d.SDLK_ESCAPE:    # move to title
-                    game_framework.pop_state()
+                    game_framework.change_state(title_state)
                 case pico2d.SDLK_b:  # resume game
-                    pass
+                    new = False
+                    game_framework.pop_state()
                 case pico2d.SDLK_r:  # restart game
-                    pass
+                    new = True
+                    game_framework.change_state(count_state)
+
+def pause():
+    pass
+
+def resume():
+    pass
