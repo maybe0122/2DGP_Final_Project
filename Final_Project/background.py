e742e3aa881d7b37bb9299d3e369222b360e6dd6
kyuminKim <kyuminkim0122@gmail.com>
2022-11-19 19:07:12 +0900

    Update background.py
    스코어 및 폰트 업데이트

--- Final_Project/background.py
+++ Final_Project/background.py
@@ -1,14 +1,15 @@
 from pico2d import *
 
+score = 0
+
 class Background:
     def __init__(self):
         self.image = load_image('source/image/background1.png')
-        self.score = 0
-        self.font = load_font('ENCR10B.TTF', 30)
+        self.font = load_font('source/font/ENCR10B.TTF', 30)
 
     def draw(self):
         self.image.draw(740 / 2, 900 / 2)
-        self.font.draw(5, 900 - 10, f'Score : {self.score}', (255, 0, 0))
+        self.font.draw(5, 900 - 10, f'Score : {score}', (255, 0, 0))
 
     def update(self):
         pass
