bd49004c6b6b6ada889e40bf12b3370e9cc9c9a8
kyuminKim <kyuminkim0122@gmail.com>
2022-12-03 18:31:34 +0900

    update enemy
    explosion animation 추가

--- Final_Project/enemy.py
+++ Final_Project/enemy.py
@@ -3,21 +3,23 @@ import game_world
 import random
 import background
 from item import Item
+from explosion import Explosion
+import game_framework
 
 import play_state
 from attack import Attack, Breath
 
+
 class Enemy1:
     image = None
 
-    def __init__(self):
-        self.x, self.y = random.randint(55, 750 - 55), 800 - 90
+    def __init__(self, x=random.randint(55, 750 - 55), y=800-90):
+        self.x, self.y = x, y
         self.dir_x = 0
         self.dir_y = 0
         self.health = 50
         self.enemy_damage = 10
         self.face_dir = -1
-
 
         if Enemy1.image == None:
             Enemy1.image = load_image('source/image/aircrafts.png')
@@ -34,19 +36,22 @@ class Enemy1:
         game_world.add_object(play_state.items, 1)
         game_world.add_collision_pairs(None, play_state.items, 'player:item')
 
+    def explosion_animation(self):
+        play_state.explosions = Explosion(self.x, self.y)
+        game_world.add_object(play_state.explosions, 1)
+
     def get_bb(self):
         return self.x - 55, self.y - 45, self.x + 55, self.y + 45
 
     def handle_collision(self, other, group):
-        print(play_state.attacks)
         if group == 'player:enemy':
             game_world.remove_object(self)
         elif group == 'attack:enemy':       # self : enemy, other : attack
             self.health -= play_state.player.player_damage
-            print(play_state.player.player_damage)
             if self.health <= 0:
                 play_state.player.score += 100
                 game_world.remove_object(self)
+                self.explosion_animation()
                 self.drop_item()
 
 class Enemy2:
