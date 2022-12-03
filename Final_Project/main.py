cb99a87ac1c6e2f9cf5da78f5c94460dfa3cb969
kyuminKim <kyuminkim0122@gmail.com>
2022-12-03 18:30:21 +0900

    Update main.py
    코드 정리

--- Final_Project/main.py
+++ Final_Project/main.py
@@ -1,22 +1,9 @@
-import play_state
 import pico2d
-import title_state
 import logo_state
 import game_framework
 
-# start_state = [logo_state, title_state, play_state]
 states = [logo_state]
-pico2d.open_canvas(740, 800)
-
-
-# for state in start_state:
-#     state.enter()  # initialize
-#     # game loop
-#     while state.running:
-#         state.handle_events()
-#         state.update()
-#         state.draw()
-#     state.exit()   # exit
+pico2d.open_canvas(740, 900)
 
 for state in states:
     game_framework.run(state)
