c384674178aad001c9e69051ceaa13252e09618f
kyuminKim <kyuminkim0122@gmail.com>
2022-12-04 06:35:02 +0900

    Update test.py

--- Final_Project/test.py
+++ Final_Project/test.py
@@ -4,6 +4,8 @@ import game_framework
 import logo_state
 import play_state
 import title_state
+import save_score_state
+import game_over_state
 
 pico2d.open_canvas(740, 900)
 game_framework.run(play_state)  # test state
