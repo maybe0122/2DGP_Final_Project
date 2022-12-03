4af6ab6dedac3d390731ae549b8eca3bb407711b
kyuminKim <kyuminkim0122@gmail.com>
2022-12-04 06:34:51 +0900

    Update logo_state.py

--- Final_Project/logo_state.py
+++ Final_Project/logo_state.py
@@ -5,13 +5,16 @@ import title_state
 image = None
 logo_time = 0.0
 
+
 def enter():
     global image
     image = load_image('source/image/tuk_credit.png')
 
+
 def exit():
     global image
     del image
+
 
 def update():
     global logo_time
@@ -21,11 +24,17 @@ def update():
     delay(0.01)
     logo_time += 0.01
 
+
 def draw():
     global image
     clear_canvas()
     image.draw(get_canvas_width() // 2, get_canvas_height() // 2)
     update_canvas()
 
+
 def handle_events():
     events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+
