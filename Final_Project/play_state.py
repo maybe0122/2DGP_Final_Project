5763bfc57bdc9cb4455a50a283e09721c967c76f
kyuminKim <kyuminkim0122@gmail.com>
2022-12-04 17:57:41 +0900

    Update play_state.py
    적군 등장 패턴 구현

--- Final_Project/play_state.py
+++ Final_Project/play_state.py
@@ -1,21 +1,55 @@
+import toml
 from pico2d import *
 import game_framework
 import game_world
 import menu_state
 import random
-
+import pickle
+import enemy
 from dragon import Dragon
-from enemy import Enemy1
+from enemy import Enemy1, Enemy2
 from background import Background
 
 
 player = None
-enemies = []
+enemies = None
 background = None
 attacks = None
 items = None
 explosions = None
+enemy_attacks = None
 
+wave_counter = 1
+enemy_counter = 0
+
+wave1 = [
+    {'name': 'Enemy1', 'x': 74, 'y': 750},
+    {'name': 'Enemy1', 'x': 74 + 148, 'y': 750},
+    {'name': 'Enemy1', 'x': 74 + 148 * 2, 'y': 750},
+    {'name': 'Enemy1', 'x': 74 + 148 * 3, 'y': 750},
+    {'name': 'Enemy1', 'x': 74 + 148 * 4, 'y': 750}
+]
+
+wave2 = [
+    {'name': "Enemy1", 'x': 123, 'y': 750},
+    {'name': "Enemy1", 'x': 369, 'y': 750},
+    {'name': "Enemy1", 'x': 615, 'y': 750},
+    {'name': "Enemy2", 'x': 123, 'y': 1600},
+    {'name': "Enemy2", 'x': 369, 'y': 1600},
+    {'name': "Enemy2", 'x': 615, 'y': 1600}
+]
+
+wave3 = [
+    {'name': "Enemy2", 'x': 74, 'y': 1600},
+    {'name': "Enemy2", 'x': 74 + 148, 'y': 1400},
+    {'name': "Enemy2", 'x': 74 + 148 * 2, 'y': 1200},
+    {'name': "Enemy2", 'x': 74 + 148 * 3, 'y': 1400},
+    {'name': "Enemy2", 'x': 74 + 148 * 4, 'y': 1600}
+]
+
+boss = [
+    {'name': 'Boss', 'x': 0, 'y': 0}
+]
 
 def handle_events():
     events = get_events()
@@ -29,30 +63,55 @@ def handle_events():
 
 
 def enter():
-    global player, enemies, background
+    global player, enemies, background, enemy_counter, wave_counter
+
+    enemy_counter = 0
+    wave_counter = 0
 
     # 객체 생성
     player = Dragon()
-    enemies = [Enemy1(random.randint(0, get_canvas_width()), 800) for _ in range(10)]
     background = Background()
 
     # 게임 월드에 객체 추가
     game_world.add_object(player, 1)
-    game_world.add_objects(enemies, 1)
     game_world.add_object(background, 0)
 
+    load_enemy(wave1)
+
     # 충돌 그룹 추가
-    game_world.add_collision_pairs(player, enemies, 'player:enemy')
-    game_world.add_collision_pairs(None, enemies, 'attack:enemy')
     game_world.add_collision_pairs(player, None, 'player:item')
-    game_world.add_collision_pairs(player, None, 'player:Enemy_attack')
+    game_world.add_collision_pairs(player, None, 'player:enemy_attack')
+    game_world.add_collision_pairs(None, None, 'attack:enemy_attack')
 
 
 def exit():
     game_world.clear()
 
 
+def load_enemy(wave):
+    global enemies, enemy_counter
+    for o in wave:
+        if o['name'] == 'Enemy1':
+            enemies = Enemy1(o['x'], o['y'])
+            game_world.add_object(enemies, 1)
+            game_world.add_collision_pairs(player, enemies, 'player:enemy')
+            game_world.add_collision_pairs(None, enemies, 'attack:enemy')
+            enemy_counter += 1
+        elif o['name'] == 'Enemy2':
+            enemies = Enemy2(o['x'], o['y'])
+            game_world.add_object(enemies, 1)
+            game_world.add_collision_pairs(player, enemies, 'player:enemy')
+            game_world.add_collision_pairs(None, enemies, 'attack:enemy')
+            enemy_counter += 1
+        # elif o['name'] == 'Boss':
+        #     enemies = Boss(o['x'], o['y'])
+        #     game_world.add_object(enemies, 1)
+        #     game_world.add_collision_pairs(player, enemies, 'player:enemy')
+        #     game_world.add_collision_pairs(None, enemies, 'attack:enemy')
+        #     enemy_counter += 1
+
 def update():
+    global wave_counter
     for game_object in game_world.all_object():
         game_object.update()
 
@@ -60,6 +119,19 @@ def update():
         if collide(a, b):
             a.handle_collision(b, group)
             b.handle_collision(a, group)
+
+    if enemy_counter == 0:
+        n = random.randint(0, 3)
+        if n == 0:
+            load_enemy(wave1)
+        if n == 1:
+            load_enemy(wave2)
+        if n == 2:
+            load_enemy(wave3)
+        wave_counter += 1
+
+    # if wave_counter % 10 == 0:
+    #     load_enemy(boss)
 
 
 def draw():
